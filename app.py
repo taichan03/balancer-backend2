4e7ab143c54f1ccf474fb03177c3ab248ed7a851
taichan03 <tai.c.chan3@gmail.com>
2023-07-05 21:48:25 -0400

    started docker, added hot reload, added endpoint for landing page.

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask
 from routes.gpt import gpt_bp
+from routes.diagnosis import diagnosis_bp
 
 app = Flask(__name__)
 
 app.register_blueprint(gpt_bp)
+app.register_blueprint(diagnosis_bp)
+
+
+@app.route('/')
+def hello_world():
+    return "Test sersver"
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
---
+++ routes/diagnosis.py
@@ -0,0 +1,25 @@
+from dotenv import load_dotenv
+import requests
+import os
+import openai
+from flask import Blueprint, request, jsonify
+
+diagnosis_bp = Blueprint('diagnosis', __name__)
+
+
+@diagnosis_bp.route('/diagnosis', methods=['POST'])
+def medication():
+    openai.api_key = os.getenv('OPENAI_API_KEY')
+    print(os.getenv('OPENAI_API_KEY'))
+    data = request.get_json()
+
+    if data is not None:
+        diagnosis = data.get('diagnosis')
+
+    ai_response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "What are the most commonly prescribed medications for %s? Exclude medications for bipolar I depression " % diagnosis, }]
+    )
+
+    return {'message': ai_response}
